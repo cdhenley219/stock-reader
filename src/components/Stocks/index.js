26c04f62105abf944f61c8fbec065ce1ff9a4ffb
cdhenley219 <cdhenley219@hotmail.com>
2022-04-17 13:21:04 -0400

    set up E2E call of removing stock

--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-//import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { popSelectedStock } from '../../store/stocksSlice';
 import './index.css';
 
 const Stocks = () => {
-   // const selectedStocks = useSelector(state => state.selectedDetailsList);
+    const selectedList = useSelector(state => state.stock.selectedDetailsList);
+    const dispatch = useDispatch();
 
-    return <div className="stocks">Stocks</div>
+    return (
+        <div className="stocks">
+        {
+            selectedList.map((item, index) => (
+                <div key={index}>
+                    <span>{item.symbol}</span>
+                    {(index === selectedList.length-1) && <button onClick={() => dispatch(popSelectedStock()) }>Remove</button>}
+                </div>
+            ))
+        }
+        </div>
+    )
 };
 
 export default Stocks;
