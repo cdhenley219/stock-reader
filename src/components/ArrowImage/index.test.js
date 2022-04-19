a1776a75ad2270b0961dce5ff213c4774b1d8ce8
cdhenley219 <cdhenley219@hotmail.com>
2022-04-19 08:00:36 -0400

    arrow updates

--- src/components/ArrowImage/index.test.js
+++ src/components/ArrowImage/index.test.js
@@ -5,11 +5,11 @@ import ArrowImage from './index';
 
 test('renders arrow up image', () => {
     render(<ArrowImage priceChange={5.00}/>);
-    expect(screen.getByRole('img', {alt:/iarrow-up/})).toBeTruthy();
+    expect(screen.getByRole('img', {alt:/iarrow up/})).toBeTruthy();
 });
 
 test('renders arrow down image', () => {
     render(<ArrowImage priceChange={-5.00}/>);
-    expect(screen.getByRole('img', {alt:/iarrow-down/})).toBeTruthy();
+    expect(screen.getByRole('img', {alt:/iarrow down/})).toBeTruthy();
 });
 
